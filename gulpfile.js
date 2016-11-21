4aac2442162098f010131ddead2040e3c6f6bc9a
ocboogie <boogie@mikulec.com>
2016-11-21 00:22:21 -0800

    Fixed commit e78ac9a and added gulp

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+gulp.task('sass', function() {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('sass:watch', function() {
+    gulp.watch(['sass/**/*.sass', 'sass/*.sass'], ['sass']);
+});
+
+gulp.task('default', ['sass', 'sass:watch']);
+/*
+gulp.task('styles', function() {
+    return gulp.src('sass/styles.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css/'));
+});
+
+
+gulp.task('default',function() {
+    gulp.watch('sass/styles.sass',['styles']);
+});
+*/
