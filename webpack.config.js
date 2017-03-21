2189dcb484e8d2d239d64d04731ec2c8d4791b8e
ocboogie <boogie@mikulec.com>
2017-03-21 00:50:46 -0700

    I forgot to build

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   entry: "./app/index.js",
 
   output: {
-    path: path.resolve(__dirname, "./"),
+    path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js"
   },
   target: 'web', 
