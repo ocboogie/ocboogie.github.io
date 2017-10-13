909e46f14af5545062ca14eb646b5940f15d0d0f
ocboogie <boogie@mikulec.com>
2017-10-12 20:21:39 -0700

    Play gif once done loading

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,15 +1,35 @@
 <template>
-  <img class="VueToNuxtLogo" src="~assets/logo.gif">
+  <div class="logo-container">
+    <div v-if="!loading">
+      <img src="~assets/logo.gif">
+    </div>
+  </div>
 </template>
 <script>
+export default {
+  data: () => ({
+    loading: true,
+  }),
+  mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 500);
+  },
+};
 </script>
 
 <style>
-.VueToNuxtLogo {
+.logo-container {
   display: inline-block;
   position: relative;
   overflow: hidden;
   height: 345px;
   width: 345px;
+}
+
+.logo-container img {
+  width: 100%;
+  height: 100%;
+
 }
 </style>
